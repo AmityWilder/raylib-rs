fc6a7271836df56315090d85e9b4a1429ad8e8a3
Henry Wilder <henrythepony@gmail.com>
2025-04-02 22:18:11 -0400

    Remove Physac from raylib-sys because the current implementation is in native Rust

--- raylib-sys/binding/physac_wrapper.c
+++
@@ -1,3 +0,0 @@
-#include "../raylib/src/raylib.h"
-#define PHYSAC_IMPLEMENTATION
-#include "Physac/src/physac.h"
--- raylib-sys/build.rs
+++ raylib-sys/build.rs
@@ -345,18 +345,6 @@ fn gen_imgui() {
         .compile("rlImGui");
 }
 
-// Physac has a compile error
-fn gen_physac() {
-    // // Compile the code and link with cc crate
-    // cc::Build::new()
-    //     .define("PHYSAC_NO_THREADS", "1")
-    //     .files(vec!["binding/physac_wrapper.c"])
-    //     .include("binding/Physac")
-    //     .warnings(false)
-    //     .extra_warnings(false)
-    //     .compile("Physac");
-}
-
 #[cfg(feature = "nobuild")]
 fn link(_platform: Platform, _platform_os: PlatformOS) {}
 
@@ -445,9 +433,6 @@ fn main() {
 
     #[cfg(feature = "imgui")]
     gen_imgui();
-
-    #[cfg(feature = "physac")]
-    gen_physac();
 }
 
 #[cfg(feature = "nobuild")]
@@ -478,9 +463,6 @@ fn main() {
 
     #[cfg(feature = "imgui")]
     gen_imgui();
-
-    #[cfg(feature = "physac")]
-    gen_physac();
 }
 
 #[must_use]
